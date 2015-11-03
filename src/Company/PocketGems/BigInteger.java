5b2a74828e2a99c49342149b14f90a740359b159
Jiadong Wu <wuplus1992@gmail.com>
2015-11-03 00:57:46 -0500

    11.3

---
+++ src/Company/PocketGems/BigInteger.java
@@ -0,0 +1,16 @@
+/*
+ Design and implement a big integer class that holds an arbitrarily-large numeric value.
+ i.e.:
+ int = 32-bit, max value = 2^32 - 1
+ long = 64-bit, max value = 2^64 - 1
+ BitInt = n-bit, max value = 2^n - 1
+ */
+package Company.PocketGems;
+
+/**
+ *
+ * @author Wu
+ */
+public class BigInteger {
+
+}
