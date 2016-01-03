f338731cef2b70c478cf8ed8444828003fae61ed
Jiadong Wu <wuplus1992@gmail.com>
2016-01-03 15:17:46 -0500

    1.3

--- src/LeetCode_Round2/Solution322.java
+++ src/LeetCode_Round2/Solution322.java
@@ -14,14 +14,13 @@ public class Solution322 {
         int[] dp = new int[amount + 1];
         for (int i = 1; i < amount + 1; i++) {
             int min = Integer.MAX_VALUE;
-            boolean b = false;
             for (int j = 0; j < length; j++) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
-                    b = true;
-                    min = Math.min(min, dp[i - coins[j]] + 1);
+                int tmp = i - coins[j];
+                if (tmp >= 0 && dp[tmp] != -1) {
+                    min = Math.min(min, dp[tmp] + 1);
                 }
             }
-            dp[i] = b ? min : -1;
+            dp[i] = (min != Integer.MAX_VALUE) ? min : -1;
         }
         return dp[amount];
     }
